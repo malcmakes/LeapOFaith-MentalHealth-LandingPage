4b9385c6be52d7272491c29c87f93a3658878d5d
Malcolm <malcolmharrellmedia@gmail.com>
2022-05-05 16:01:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LeapOFaith-MentalHealth-LandingPage
---
+++ script.js
@@ -0,0 +1,54 @@
+
+const navBurger = () => {
+
+
+const headerNew = document.querySelector('header')
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links-li');
+
+
+burger.addEventListener('click', () => {
+   const navActiveState= nav.classList.toggle('nav-active');
+
+
+    if (navActiveState) {
+
+        headerNew.style.backgroundColor = "#43aa8b";
+        headerNew.style.width = "100%";
+        console.log('background changed')
+    }
+
+        else {
+            headerNew.style.backgroundColor = "transparent";
+            console.log('background normal')
+        }
+
+ 
+    navLinks.forEach((link,index) => {
+        if (link.style.animation){
+            link.style.animation =''
+            
+            
+        }
+        else {
+           
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.4}s`;
+            
+        
+        
+        }
+        console.log(index / 7);
+
+
+        if (link.style.animation) {
+           
+        }
+    })
+
+
+})
+
+}
+
+navBurger()
